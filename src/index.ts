b1cc0ff554335ea79151f7128e1847b87b4b0b5d
Albin Medoc <stamline203@gmail.com>
2022-11-04 22:52:27 +0100

    Cleanup

--- src/index.ts
+++ src/index.ts
@@ -1,17 +1,22 @@
 import Cleanmate from './cleanmateService';
+import CleanmateMap from './cleanmateMap';
 import {
   WorkMode,
   WorkState,
   MopMode,
 } from './types';
-import type { CleanmateStatus } from './types';
+import type { Status, MapData, Point, Region } from './types';
 
 export default Cleanmate;
 export {
+  CleanmateMap,
   WorkMode,
   WorkState,
   MopMode,
 };
 export type {
-  CleanmateStatus,
+  Status,
+  MapData,
+  Point,
+  Region,
 };
